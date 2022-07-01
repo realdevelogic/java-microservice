1d7ca8d36ebbf310add9be8665b5f7016008c64b
Volentis <alirezamovahedifar@gmail.com>
2022-07-02 03:46:16 +0430

    added customer service

--- customer/src/main/java/com/volentis/customer/CustomerApplication.java
+++ customer/src/main/java/com/volentis/customer/CustomerApplication.java
@@ -1,2 +1,11 @@
-package main.java.com.volentis.customer;public class CustomerApplication {
+package com.volentis.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
 }
