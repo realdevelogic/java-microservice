54ef931f8d199bcf7f680a22d290130ac5703be7
Volentis <alirezamovahedifar@gmail.com>
2022-07-02 04:09:28 +0430

    added  CustomerService

--- customer/src/main/java/com/volentis/customer/Customer.java
+++ customer/src/main/java/com/volentis/customer/Customer.java
@@ -1,2 +1,13 @@
-package com.volentis.customer;public class Customer {
+package com.volentis.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
