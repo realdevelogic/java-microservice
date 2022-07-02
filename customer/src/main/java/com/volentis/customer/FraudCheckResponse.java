b9266d9d53df9975180ae8e2f7ff6819fee68ba1
Volentis <alirezamovahedifar@gmail.com>
2022-07-02 21:47:05 +0430

    added eureka service discovery server

---
+++ customer/src/main/java/com/volentis/customer/FraudCheckResponse.java
@@ -0,0 +1,2 @@
+package com.volentis.customer;public class FraudCheckResponse {
+}
---
+++ eureka-server/src/main/java/com/volentis/App.java
@@ -0,0 +1,13 @@
+package com.volentis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
