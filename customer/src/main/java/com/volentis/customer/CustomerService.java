3640f6be68fa1e45a154f0a008deb809e276b5b7
Volentis <alirezamovahedifar@gmail.com>
2022-07-02 15:31:00 +0430

    added CustomerRepository and modified CustomerService

--- customer/src/main/java/com/volentis/customer/CustomerService.java
+++ customer/src/main/java/com/volentis/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.volentis.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -14,7 +14,7 @@ public record CustomerService() {
 
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
 
     }
 }
