cdbeb3d954acdf1c537e64c3a6cd31b8c997cc00
Volentis <alirezamovahedifar@gmail.com>
2022-07-03 01:28:36 +0430

    added service discovery annotations to fraud and customer services

--- fraud/src/main/java/com/volentis/fraud/FraudApplication.java
+++ fraud/src/main/java/com/volentis/fraud/FraudApplication.java
@@ -3,10 +3,15 @@ package com.volentis.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.util.SocketUtils;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
     public static void main(String[] args) {
+        int availablePort = SocketUtils.findAvailableTcpPort(8080, 8099);
+        System.setProperty("server.port", String.valueOf(availablePort));
         SpringApplication.run(FraudApplication.class, args);
     }
 }
